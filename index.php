c5a723b31214f73b3c70d695f95b88c50ee4a24e
31448471 <31448471@087872-P33S105.macklabs.br>
2014-08-29 08:41:43 -0300

    exercicio 5.1

--- index.php
+++ index.php
@@ -35,8 +35,63 @@
          else if($MF < 5.0){
              echo "reprovado";
          }
-        
-        
         ?>
-    </body>
+        
+        </br>
+        <b>Exercicio 5.1</b>
+        <?php
+        
+        //Array turmas
+        $turma[0]= "02K01";
+        $turma[1]= "02J01";
+        
+        //Array Disciplinas
+        $disciplinas[0]= "Tecnologia Web 2";
+        $disciplinas[1]= "Linguagem de Programação 1";
+        
+        //Array Semana
+        $semana[0]= "Segunda" ;
+        $semana[1]= "Terça"   ;
+        $semana[2]= "Quarta"  ;
+        
+        //Array professor
+        $professor[0]=  "João"   ;
+        $professor[1]= "Charles" ;
+             
+        
+        
+        echo 
+            " <table border='1'>
+            <tr>
+              <th>Turma</th>
+              <th>Disciplina</th>
+              <th>Semana</th>
+              <th>Professor</th>
+            </tr>    
+           
+            <tr>
+                <td>$turma[0]</td>
+                <td>$disciplinas[0]</td>
+                <td>$semana[0]</td>
+                <td>$professor[1]</td>
+            </tr> 
+            
+            <tr>
+                <td>$turma[1]</td>
+                <td>$disciplinas[1]</td>
+                <td>$semana[1]</td>
+                <td>$professor[0]</td>
+            </tr> 
+            
+
+            <tr>
+                <td>$turma[0]</td>
+                <td>$disciplinas[1]</td>
+                <td>$semana[2]</td>
+                <td>$professor[1]</td>
+            </tr> 
+        </table>"
+                
+        ?>   
+    </body> 
 </html>
